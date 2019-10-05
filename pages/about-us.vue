80e0384945f7d04069e657555ffc7f5f8ca62c90
André Domingues <voxivoid@gmail.com>
2019-10-05 10:50:03 +0100

    feat: started about us

---
+++ pages/about-us.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+.about-us.simplebar
+  h1 {{ $t("aboutUs") }}
+  p {{ aboutUs[locale] }}
+</template>
+
+<script>
+import simplebar from "simplebar-vue";
+
+import LocaleHelpers from "@/mixins/helpers.locale";
+
+import aboutUs from "@/mocks/about-us";
+
+export default {
+  components: {
+    simplebar,
+  },
+  mixins: [LocaleHelpers],
+  data() {
+    return {
+      aboutUs,
+    };
+  },
+
+  i18n: {
+    messages: {
+      en: {
+        aboutUs: "About us",
+      },
+      pt: {
+        aboutUs: "Sobre nós",
+      },
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~assets/breakpoints'
+@import '~assets/colors'
+
+.about-us
+  color $colors-white
+  background $colors-black
+  padding 64px
+
+</style>
