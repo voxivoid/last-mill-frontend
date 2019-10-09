1922166e44fe34a0214a47dfb614a227bfe71a3d
André Domingues <voxivoid@gmail.com>
2019-10-09 19:08:43 +0100

    fix: margins

--- layouts/default.vue
+++ layouts/default.vue
@@ -60,11 +60,13 @@ html
   -webkit-font-smoothing antialiased
   box-sizing border-box
 
+body
+  margin 0
+
 *,
 *:before,
 *:after
   box-sizing border-box
-  margin 0
 
 a
   text-decoration none
