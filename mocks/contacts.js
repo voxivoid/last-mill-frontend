b8a1572ecb46d198715db929817722c85c54825c
André Domingues <voxivoid@gmail.com>
2019-10-15 21:06:51 +0100

    feat: contacts mock

---
+++ mocks/contacts.js
@@ -0,0 +1,7 @@
+export default {
+  email: "lastmill@lastmill.com",
+  phones: ["+351 910 282 480", "+351 933 355 095"],
+  address: "Praceta Quinta Nova 9A Loja, 2700-688 Amadora",
+  facebook: "https://www.facebook.com/lastmillcompany/",
+  instagram: "https://www.instagram.com/lastmillcompany/",
+};
