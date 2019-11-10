39d7f97fad8d0d862e2cbbd3e926ecbdbc3413d7
André Domingues <voxivoid@gmail.com>
2019-11-10 18:06:23 +0000

    feat: added homepage video

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
 .home
   logo-horizontal.logo
+  .overlay
+  video(width="100%" autoplay loop muted)
+    source(src="/videos/home_720p.mp4" type="video/mp4")
 </template>
 
 <script>
@@ -17,13 +20,32 @@ export default {
 @import '~assets/colors'
 
 .home
+  position relative
   display grid
   place-items center center
   background $colors-black
   height 100%
   width 100%
   padding 32px
+  overflow hidden
+
+.overlay
+  position absolute
+  top 0
+  bottom 0
+  left 0
+  right 0
+  z-index 1
+  background linear-gradient(90deg, rgba(0,0,0,0) 0%, $colors-black 100%);
 
 .logo
   max-width 512px
+  z-index 2
+
+video
+  position absolute
+  top 0
+  bottom 0
+  left 0
+  right 0
 </style>
